a8d01c443bd5aa3a2ff16fdd82eacc45a0b6a423
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-04-24 00:34:19 +0900

    Modify import statements

--- 2.x/sampleProject/proj.android/src/com/growthpush/cocos2dx/sample/sampleProject.java
+++ 2.x/sampleProject/proj.android/src/com/growthpush/cocos2dx/sample/sampleProject.java
@@ -20,33 +20,35 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-****************************************************************************/
-package jp.co.sirok.cocos2d2sample;
+ ****************************************************************************/
+package com.growthpush.cocos2dx.sample;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 import android.os.Bundle;
 
+import com.growthpush.cocos2dx.GrowthPushJNI;
+
 public class sampleProject extends Cocos2dxActivity {
-	
+
 	static GrowthPushJNI growthpush;
-	
-    protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);	
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		growthpush = new GrowthPushJNI(getApplicationContext());
 	}
 
-    public Cocos2dxGLSurfaceView onCreateView() {
-    	Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
-    	// sampleProject should create stencil buffer
-    	glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
-    	
-    	return glSurfaceView;
-    }
+	public Cocos2dxGLSurfaceView onCreateView() {
+		Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+		// sampleProject should create stencil buffer
+		glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
 
-    static {
-        System.loadLibrary("cocos2dcpp");
-    }     
-    
+		return glSurfaceView;
+	}
+
+	static {
+		System.loadLibrary("cocos2dcpp");
+	}
+
 }
--- 3.0/sampleProject/proj.android/src/com/growthpush/cocos2dx/GrowthPushJNI.java
+++ 3.0/sampleProject/proj.android/src/com/growthpush/cocos2dx/GrowthPushJNI.java
@@ -76,8 +76,6 @@ public class GrowthPushJNI {
 
 	}
 
-
-
 	/*
 	 * Remote notification callback method (JNI interface)
 	 * 
